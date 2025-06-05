a6c890bb46f504343b92ba384eb0ad60855cee63
Rohit Mishra <mishrarohith1402@gmail.com>
2025-06-05 22:27:06 +0530

    initial commit

---
+++ lib/app/data/graphql/graphql_client.dart
@@ -0,0 +1,14 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class GraphQLService {
+  static GraphQLClient initClient() {
+    final HttpLink httpLink = HttpLink(
+      'https://spacex-production.up.railway.app/',
+    );
+
+    return GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(store: InMemoryStore()),
+    );
+  }
+}
